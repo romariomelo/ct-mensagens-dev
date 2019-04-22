4e4bb36f1d9f45d8e4b3a8fe4e7a86fa63668593
romariomelo <romario.melos@gmail.com>
2019-04-21 22:02:40 -0300

    Caixa de Entrada

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist\\js')
     }
 };
 
-module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    output: {
-        filename: 'main.js',
-        path: 'C:\\ProjetosWeb\\colegiotimbaubense\\professor\\node_modules\\ct-mensagem\\dist\\js'
-    }
-};
+// module.exports = {
+//     entry: './src/index.js',
+//     mode: 'development',
+//     output: {
+//         filename: 'main.js',
+//         path: 'C:\\ProjetosWeb\\colegiotimbaubense\\professor\\node_modules\\ct-mensagem\\dist\\js'
+//     }
+// };
