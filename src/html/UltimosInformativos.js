a572a3f7949b87fa77b1eb2d0bc650d594c29ac0
romariomelo <romario.melos@gmail.com>
2019-09-10 11:56:44 -0300

    Signed-off-by: romariomelo <romario.melos@gmail.com>

--- src/html/UltimosInformativos.js
+++ src/html/UltimosInformativos.js
@@ -1,28 +1,5 @@
 ultimosInformativos = function(mensagens, config) {
-    // var html = '';
-    // mensagens.forEach((mensagem, index) => {
-    //     if ( index > 4 ) {
-    //         return;
-    //     }
-    //     html += itemUltimosInformativos(mensagem, config);
-    // })
-    // html += `
-    //         <li>
-    //             <div class="text-center link-block">
-    //                 <a href="${config.base_url}/avisos/informativos" class="dropdown-item">
-    //                     <strong>Ver todas</strong>
-    //                     <i class="fa fa-angle-right"></i>
-    //                 </a>
-    //             </div>
-    //         </li>
-    //         `
-    // return html;
-
-
-
     let listElement = [];
-
-
 
     mensagens.forEach((mensagem, index) => {
         if ( index > 4 ) {
@@ -40,6 +17,7 @@ ultimosInformativos = function(mensagens, config) {
     let a = document.createElement('a');
     a.setAttribute('href', config.base_url +'/avisos/informativos');
     a.setAttribute('class', 'dropdown-item');
+    a.setAttribute('style', 'text-align: center');
     a.innerHTML = '<strong>Ver todas</strong> <i class="fa fa-angle-right"></i>';
 
     liVerMais.appendChild(a);
@@ -52,20 +30,6 @@ ultimosInformativos = function(mensagens, config) {
 itemUltimosInformativos = function(mensagem, config) {
     var classNoRead = (mensagem.read_at === null) ? 'no-read' : ''
     var foto = (mensagem.alerta.user.foto === null) ? config.base_url + '/images/sem_foto.jpg' : config.webservice_url + '/Usuario/' + mensagem.alerta.user.foto
-    // var html = `
-    //                 <li class="${classNoRead} notification-item" data-codigo="${mensagem.alerta.codigo}">
-    //                     <div class="dropdown-messages-box">
-    //                         <a class="dropdown-item pull-left" style="padding: 5px">
-    //                             <img alt="" class="img-circle" src="${foto}">
-    //                         </a>
-    //                         <div class="pull-right" style="width:80%">
-    //                             Mensagem de <strong>${mensagem.alerta.user.name}</strong>: ${mensagem.alerta.titulo}.<br>
-    //                             <small class="text-muted">Em ` + moment(mensagem.alerta.created_at).format('DD/MM/YYYY') + `</small>
-    //                         </div>
-    //                     </div>
-    //                 </li>
-    //                 <li role="separator" class="dropdown-divider divider"></li>`;
-    // return html;
 
     let li = document.createElement('li');
     li.setAttribute('class', `${classNoRead} notification-item`);
